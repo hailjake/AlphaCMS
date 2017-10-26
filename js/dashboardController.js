12d346b642322ff55350ad9847d9dee90cf88d66
jake <inhailmedia@gmail.com>
2017-10-26 14:01:12 -0700

    add firebase db connection
    ftw

--- js/dashboardController.js
+++ js/dashboardController.js
@@ -1,11 +1,24 @@
 
-nextGenApp.controller("dashboardController", ["$scope", "Auth",
- function($scope, Auth, $firebaseAuth) {
+nextGenApp.controller("dashboardController", ["$scope", "Auth", "$firebaseArray",
+ function($scope, Auth, $firebase, $firebaseAuth, $firebaseArray) {
     $scope.auth = Auth;
+    var db = firebase.firestore();
+
+
+    db.collection("content").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+            
+            $scope.database = doc.data();
+            $scope.$apply()
+        });
+    });
 
     // any time auth state changes, add the user data to scope
     $scope.auth.$onAuthStateChanged(function(firebaseUser) {
      $scope.firebaseUser = firebaseUser;
     });
+     
+     
   }
 ]);
