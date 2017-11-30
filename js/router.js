406826e6366d88a42d85dc8f149d224a980c96a3
jake <inhailmedia@gmail.com>
2017-11-30 14:15:08 -0800

    add nested routing
    for editor pages and links in main dashboard

--- js/router.js
+++ js/router.js
@@ -1,13 +1,16 @@
 var nextGenApp = angular.module('nextGenApp', ['ngResource', 'ui.router', 'ngStorage', 'firebase']);
 
-nextGenApp.run(["$rootScope", "$state", function($rootScope, $state) {
-  $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
-    // We can catch the error thrown when the $requireSignIn promise is rejected
-    // and redirect the user back to the home page
-    if (error === "AUTH_REQUIRED") {
-      $state.go("/");
-    }
-  });
+
+nextGenApp.run(['$rootScope', '$state', function ($rootScope, $state) {
+
+    $rootScope.$on('$stateChangeStart', function (evt, to, params) {
+        if (to.redirectTo) {
+            evt.preventDefault();
+            $state.go(to.redirectTo, params, {
+                location: 'replace'
+            })
+        }
+    });
 }]);
 
 
@@ -16,33 +19,49 @@ nextGenApp.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-    // Main Index
-    .state('home', {
-        url: '/',
-        templateUrl: 'home.html',
-        controller: 'homeController'
-    })
-    
-    .state('dashboard', {
-        url: '/dashboard',
-        templateUrl: 'dashboard.html',
-        controller: 'dashboardController',
-      resolve: {
-        // controller will not be loaded until $requireSignIn resolves
-        // Auth refers to our $firebaseAuth wrapper in the factory below
-        "currentAuth": ["Auth", function(Auth) {
-          // $requireSignIn returns a promise so the resolve waits for it to complete
-          // If the promise is rejected, it will throw a $stateChangeError (see above)
-          return Auth.$requireSignIn();
-        }]
-      }
-    });
+        // Main Index
+        .state('home', {
+            url: '/',
+            templateUrl: 'home.html',
+            controller: 'homeController'
+        })
+
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'dashboard.html',
+            controller: 'dashboardController',
+            redirectTo: 'dashboard.main'
+        })
+        .state('dashboard.main', {
+            url: '/main',
+            templateUrl: 'dashboard.main.html',
+            controller: 'dashboardController'
+        })
+        .state('dashboard.contact', {
+            url: '/contact',
+            templateUrl: 'dashboard.contact.html',
+            controller: 'dashboardController'
+        })
+        .state('dashboard.about', {
+            url: '/about',
+            templateUrl: 'dashboard.about.html',
+            controller: 'dashboardController'
+        })
+        .state('dashboard.services', {
+            url: '/services',
+            templateUrl: 'dashboard.services.html',
+            controller: 'dashboardController'
+        })
+        .state('dashboard.blog', {
+            url: '/blog',
+            templateUrl: 'dashboard.blog.html',
+            controller: 'dashboardController'
+        });
 
 });
 
 nextGenApp.factory("Auth", ["$firebaseAuth",
-  function($firebaseAuth) {
-    return $firebaseAuth();
+  function ($firebaseAuth) {
+        return $firebaseAuth();
   }
 ]);
-
