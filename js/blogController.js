ba76de77d7df07ebe16e8a0bb2cd66e6b9e959c7
jake <inhailmedia@gmail.com>
2017-12-04 17:14:10 -0800

    wip blog section
    cmon!

--- js/blogController.js
+++ js/blogController.js
@@ -1,6 +1,33 @@
 nextGenApp.controller("blogController", ["$scope", "$state", "Auth", "$firebaseArray",
  function ($scope, $state, Auth, $firebase, $firebaseAuth, $firebaseArray, $timeout) {
-       console.log('blog loaded');
+        console.log('blog loaded');
+        // Firestore
+        var db = firebase.firestore();
+        $scope.database = [];
+        $scope.database.Title = "";
+        $scope.database.Post = "";
 
-       
+     
+        // Get Data
+        db.collection("blog").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                $scope.database = doc.data();
+                console.log($scope.database);
+                $scope.$apply();
+            });
+        });
+
+     
+     
+
+        // Update Data
+        $scope.updateData = function () {
+            db.collection("blog").add({
+                 Title: "31eee",
+                Post: "3eee",
+                UserPosted: "jeeed"
+        
+            })
+        };
+
  }]);
